18aad72d22085ca68f70336763b674b1ab7a0bfc
Thiago soares <104157930+0thiago@users.noreply.github.com>
2022-10-11 12:26:26 -0300

    get method working with and without id

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,7 +1,20 @@
 const ProductsModel = require('../models/products')
 
-async function get(req,res) {
-  const products = await ProductsModel.find() 
+async function get(req, res) {
+  const { id } = req.params
+
+  // I did:
+  // let products
+  // if (id) {
+  //   products = await ProductsModel.find({ _id: id })
+  // } else {
+  //   products = await ProductsModel.find()
+  // }
+
+  // TERNARY IF
+  const obj = id ? { _id: id } : null 
+
+  const products = await ProductsModel.find(obj)
 
   res.send(products)
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,9 +2,12 @@ const router = require('express').Router()
 
 const ProdutctController = require('../controllers/products')
 
-router.get('/products', ProdutctController.get)
+router.get('/products/:id?', ProdutctController.get) //? means optional on express
+
 // router.post('/products', ProdutctController.post)
+
 // router.put('/products/:id', ProdutctController.put)
+
 // router.delete('/products/:id', ProdutctController.delete)
 
 module.exports = router
