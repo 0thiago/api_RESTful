49fb91d9862d643395d871a7ec1657c449b1398d
Thiago soares <104157930+0thiago@users.noreply.github.com>
2022-10-11 21:59:34 -0300

    adding cors

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-// const path = require('path')
+const cors = require('cors')
 
 const db = require('./database/db')
 const routes = require('./routes/routes')
@@ -8,12 +8,30 @@ const app = express()
 
 db.connect()
 
+const allowedOrigins = [
+  'http://127.0.0.1:5500',
+  'http://www.app.com.br'
+]
+
+//it allows cors
+app.use(cors({
+  origin: function (origin, callback) {
+    let allowed = true
+
+    if (!origin) allowed = true
+
+    if (!allowedOrigins.includes(origin)) allowed = false
+
+    callback(null, allowed)
+  }
+}))
+
 //it allows server receive JSON format data
 app.use(express.json())
 
 app.use('/api', routes)
 
 const port = process.env.PORT || 8080
-app.listen(port, () => { 
-  console.log('The server is running on port: '+port) 
+app.listen(port, () => {
+  console.log('The server is running on port: ' + port)
 })
